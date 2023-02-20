58d9ae7e843b99c2bd6b50472b3400745f236610
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-02-20 12:15:44 +0100

    Refactor options setup a litte

--- lib/vident/root_component/using_view_component.rb
+++ lib/vident/root_component/using_view_component.rb
@@ -19,24 +19,27 @@ if Gem.loaded_specs.has_key? "view_component"
         end
 
         def call
-          # Capture inner block content
-          # It's important that we capture the block content before generating the tag options.
-          # This is because the content could contain calls to `s.data_map`.
-          # These calls add key-value pairs to the internal data_map, which can then be translated into
-          # the correct `data-*` attrs by the tag options.
-          generated = content
-
           # Generate outer tag options and render
-          tag_type = @element_tag.presence || :div
-          options = @html_options&.dup || {}
-          data_attrs = tag_data_attributes
-          options[:data] = options[:data].present? ? data_attrs.merge(options[:data]) : data_attrs
-          options = options.merge(id: @id) if @id
+          tag_type = content_tag_type
+          options = content_tag_options
           if SELF_CLOSING_TAGS.include?(tag_type)
             view_context.tag(tag_type, options)
           else
-            view_context.content_tag(tag_type, generated, options)
+            view_context.content_tag(tag_type, content, options)
           end
+        end
+
+        private
+
+        def content_tag_options
+          options = @html_options&.dup || {}
+          data_attrs = tag_data_attributes
+          options[:data] = options[:data].present? ? data_attrs.merge(options[:data]) : data_attrs
+          options.merge(id: @id) if @id
+        end
+
+        def content_tag_type
+          @element_tag.presence || :div
         end
       end
     end
