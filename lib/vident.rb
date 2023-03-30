bd7d057ab311493ef9c4ba424209b5fc30bb91a6
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-03-30 17:53:01 +0200

    Remove unused configuration

--- lib/vident.rb
+++ lib/vident.rb
@@ -4,22 +4,5 @@ require "vident/version"
 require "vident/engine"
 
 module Vident
-  class << self
-    def configuration
-      @configuration ||= Configuration.new
-    end
-
-    def configure
-      yield(configuration) if block_given?
-      configuration
-    end
-  end
-
-  class Configuration
-    attr_accessor :include_i18n_helpers
-
-    def initialize
-      @include_i18n_helpers = true
-    end
-  end
+  # Your code goes here...
 end
