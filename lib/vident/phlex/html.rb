ab80e7baa8f8d94eb1206bbda98f33ad9c9f9c89
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-04-01 10:50:42 +0200

    Use new Component method

--- lib/vident/phlex/html.rb
+++ lib/vident/phlex/html.rb
@@ -17,14 +17,7 @@ module Vident
 
       # Helper to create the main element
       def parent_element(**options)
-        @parent_element ||= begin
-          element_attrs = options
-            .except(:id, :element_tag, :html_options, :controller, :controllers, :actions, :targets, :named_classes, :data_maps)
-            .merge(
-              stimulus_options_for_component(options)
-            )
-          RootComponent.new(**element_attrs)
-        end
+        @parent_element ||= RootComponent.new(**parent_element_attributes(options))
       end
       alias_method :root, :parent_element
     end
