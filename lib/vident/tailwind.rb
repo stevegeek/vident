4023d0ec68c59ec9838a762a4b969b3c98032f98
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-04-02 07:14:55 +0200

    Only call tailwind_merge with populated strings of classes

--- lib/vident/tailwind.rb
+++ lib/vident/tailwind.rb
@@ -8,7 +8,8 @@ module Vident
     # Adds a utility class merge specifically for Tailwind, allowing you to more easily specify class overrides
     # without having to worry about the specificity of the classes.
     def produce_style_classes(class_names)
-      ::TailwindMerge::Merger.new.merge(dedupe_view_component_classes(class_names))
+      to_merge = dedupe_view_component_classes(class_names)
+      ::TailwindMerge::Merger.new.merge(to_merge) if to_merge.present?
     end
   end
 end
