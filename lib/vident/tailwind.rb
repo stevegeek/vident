384d58aae50b0c45f63c1c6fd8d4639ea32a30ec
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-08-11 11:43:39 +0200

    Only 1 merger per thread

--- lib/vident/tailwind.rb
+++ lib/vident/tailwind.rb
@@ -4,12 +4,20 @@ require "vident/tailwind/railtie"
 require "tailwind_merge"
 
 module Vident
+  # Adds a utility class merge specifically for Tailwind, allowing you to more easily specify class overrides
+  # without having to worry about the specificity of the classes.
   module Tailwind
-    # Adds a utility class merge specifically for Tailwind, allowing you to more easily specify class overrides
-    # without having to worry about the specificity of the classes.
     def produce_style_classes(class_names)
       to_merge = dedupe_view_component_classes(class_names)
-      ::TailwindMerge::Merger.new.merge(to_merge) if to_merge.present?
+      tailwind_class_merger.merge(to_merge) if to_merge.present?
+    end
+
+    private
+
+    # If needed this can be overridden in your component to provide options for the merger.
+    def tailwind_class_merger
+      return @tailwind_class_merger if defined? @tailwind_class_merger
+      @tailwind_class_merger= (Thread.current[:tailwind_class_merger] ||= ::TailwindMerge::Merger.new)
     end
   end
 end
