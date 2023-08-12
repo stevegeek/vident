94d97e78f37261c52849c76f56e5ec25c2eb20a6
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-08-12 08:39:34 +0200

    Add phlex core to be used by vident-phlex and vident-typed-phlex

--- lib/vident/phlex/html.rb
+++ lib/vident/phlex/html.rb
@@ -2,15 +2,10 @@
 
 module Vident
   module Phlex
-    class HTML < ::Phlex::HTML
+    class HTML < Core
       include ::Vident::Component
 
       class << self
-        def inherited(subclass)
-          subclass.component_source_file_path = caller_locations(1, 10).reject { |l| l.label == "inherited" }[0].path
-          super
-        end
-
         # Phlex uses a DSL to define the document, and those methods could easily clash with our attribute
         # accessors. Therefore in Phlex components we do not create attribute accessors, and instead use instance
         # variables directly.
@@ -18,22 +13,7 @@ module Vident
           options[:delegates] = false
           super
         end
-
-        attr_accessor :component_source_file_path
-
-        # Caching support
-        def current_component_modified_time
-          path = component_source_file_path
-          raise StandardError, "No component source file exists #{path}" unless path && ::File.exist?(path)
-          ::File.mtime(path).to_i.to_s
-        end
       end
-
-      # Helper to create the main element
-      def parent_element(**options)
-        @parent_element ||= RootComponent.new(**parent_element_attributes(options))
-      end
-      alias_method :root, :parent_element
     end
   end
 end
