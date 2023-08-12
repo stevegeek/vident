9b0916ed7f642fcd2e6a874022bc47cc2fab8908
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-08-12 07:37:36 +0200

    Use the new core 'base' class

--- lib/vident/typed/view_component/base.rb
+++ lib/vident/typed/view_component/base.rb
@@ -1,14 +1,8 @@
 module Vident
   module Typed
     module ViewComponent
-      class Base < ::ViewComponent::Base
+      class Base < ::Vident::ViewComponent::Core
         include ::Vident::Typed::Component
-
-        # Helper to create the main element
-        def parent_element(**options)
-          @parent_element ||= ::Vident::ViewComponent::RootComponent.new(**parent_element_attributes(options))
-        end
-        alias_method :root, :parent_element
       end
     end
   end
