963c1d2b0c58be6b3c156932955624f6aae14282
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-07-03 17:57:36 +0200

    Using literal now

--- lib/vident/phlex/html.rb
+++ lib/vident/phlex/html.rb
@@ -4,16 +4,6 @@ module Vident
   module Phlex
     class HTML < Core
       include ::Vident::Component
-
-      class << self
-        # Phlex uses a DSL to define the document, and those methods could easily clash with our attribute
-        # accessors. Therefore in Phlex components we do not create attribute accessors, and instead use instance
-        # variables directly.
-        def attribute(name, **options)
-          options[:delegates] = false
-          super
-        end
-      end
     end
   end
 end
